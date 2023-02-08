703fb0a4b88e4929c77d48a4510954dae3955181
LukasJandl <jandl.lukas@gmail.com>
2023-02-08 17:36:06 +0100

    feat: add weather cards with dummy data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,26 @@
-import Header from "./components/Header"
+import { useState } from "react";
+
+import Header from "./components/Header";
 import Footer from "./components/Footer";
+import WeatherCards from "./components/WeatherCards";
 
 function App() {
-  return (
+    const [weatherCards, setWeatherCards] = useState([
+        {
+            id: 1,
+            location: "Vienna",
+            weatherCode: 0,
+            temperature: "10°C",
+            windSpeed: "5km/h",
+        }
+    ])
+    return (
     <div className="App">
         <Header title="Weather Tracker"/>
+        <WeatherCards weatherCards={weatherCards}/>
         <Footer />
     </div>
-  );
+    );
 }
 
 export default App;
