df9918d5cfa9d9e2abbf320315f0d726133d2320
jurijkreutz <jurij.kreutz97@gmail.com>
2023-02-09 11:08:27 +0100

    refactor: clean up WeatherFetcher.js

--- frontend/src/components/WeatherFetcher.js
+++ frontend/src/components/WeatherFetcher.js
@@ -1,16 +1,13 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 
 export default function WeatcherFetcher(location, setWeatherCards) {
     useEffect(() => {
         const fetchWeather = async () =>  {
             const coordinatesRes = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${location}&language=de`)
             const coordinatesData = await coordinatesRes.json()
-            const coordinates = {
-                name: coordinatesData.results[0].name,
-                latitude: coordinatesData.results[0].latitude,
-                longitude: coordinatesData.results[0].longitude
-            }
-            const weatherRes = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${coordinates.latitude}&longitude=${coordinates.longitude}&hourly=temperature_2m,weathercode,windspeed_10m&current_weather=true`)
+            const latitude = coordinatesData.results[0].latitude
+            const longitude = coordinatesData.results[0].longitude
+            const weatherRes = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weathercode,windspeed_10m&current_weather=true`)
             const weatherData = await weatherRes.json()
             setWeatherCards([{
                 id: 1,
