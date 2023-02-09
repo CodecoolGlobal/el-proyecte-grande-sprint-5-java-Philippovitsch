bf7fa2a8445e43cfc6232e6995c21e3a6142377b
jurijkreutz <jurij.kreutz97@gmail.com>
2023-02-09 13:49:33 +0100

    refactor: remove unnecessary api url part

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,23 +3,15 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import WeatherCards from "./components/WeatherCards";
-import WeatcherFetcher from "./components/WeatherFetcher";
+import WeatherFetcher from "./components/WeatherFetcher";
 
 function App() {
 
     const location = 'Vienna'
 
-    let [weatherCards, setWeatherCards] = useState([
-        {
-            id: 1,
-            location: location,
-            weatherCode: 0,
-            temperature: "",
-            windSpeed: "",
-        }
-    ])
+    let [weatherCards, setWeatherCards] = useState([])
 
-    WeatcherFetcher(location, setWeatherCards)
+    WeatherFetcher(location, setWeatherCards)
 
     return (
     <div className="App">
--- frontend/src/components/WeatherFetcher.js
+++ frontend/src/components/WeatherFetcher.js
@@ -1,14 +1,13 @@
 import { useEffect } from 'react';
 
-export default function WeatcherFetcher(location, setWeatherCards) {
+export default function WeatherFetcher(location, setWeatherCards) {
     useEffect(() => {
         const fetchWeather = async () =>  {
             const coordinatesRes = await fetch(`/api/coordinates/${location}`)
             const coordinatesData = await coordinatesRes.json()
-            console.log(coordinatesData)
             const latitude = coordinatesData[0].latitude
             const longitude = coordinatesData[0].longitude
-            const weatherRes = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weathercode,windspeed_10m&current_weather=true`)
+            const weatherRes = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`)
             const weatherData = await weatherRes.json()
             setWeatherCards([{
                 id: 1,
