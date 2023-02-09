3ac0129293f91faf5a43cfba1cd70fefb1c5bd6b
jurijkreutz <jurij.kreutz97@gmail.com>
2023-02-09 13:34:36 +0100

    feat: add internal coordinates API

--- frontend/src/components/WeatherFetcher.js
+++ frontend/src/components/WeatherFetcher.js
@@ -3,10 +3,11 @@ import { useEffect } from 'react';
 export default function WeatcherFetcher(location, setWeatherCards) {
     useEffect(() => {
         const fetchWeather = async () =>  {
-            const coordinatesRes = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${location}&language=de`)
+            const coordinatesRes = await fetch(`/api/coordinates/${location}`)
             const coordinatesData = await coordinatesRes.json()
-            const latitude = coordinatesData.results[0].latitude
-            const longitude = coordinatesData.results[0].longitude
+            console.log(coordinatesData)
+            const latitude = coordinatesData[0].latitude
+            const longitude = coordinatesData[0].longitude
             const weatherRes = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,weathercode,windspeed_10m&current_weather=true`)
             const weatherData = await weatherRes.json()
             setWeatherCards([{
