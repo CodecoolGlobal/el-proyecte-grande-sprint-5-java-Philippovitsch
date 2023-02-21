b9a453e9d87d0e177f67ac7152a3bd352a174c9f
jurijkreutz <jurij.kreutz97@gmail.com>
2023-02-21 15:00:15 +0100

    fix: separate cardwrapper from cards

--- frontend/src/components/WeatherCards.jsx
+++ frontend/src/components/WeatherCards.jsx
@@ -2,10 +2,10 @@ import WeatherCard from "./WeatherCard";
 
 export default function WeatherCards({ weatherCards }) {
   return (
-    <>
+    <div class="cardwrapper">
       {weatherCards.map((card) => (
         <WeatherCard key={card.id} card={card} />
       ))}
-    </>
+    </div>
   );
 }
