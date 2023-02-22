5955b11d12c759a52a9bab6c93c58275fe17d364
LukasJandl <jandl.lukas@gmail.com>
2023-02-22 10:58:51 +0100

    refactor: use WebClient for API REST-requests

--- backend/src/main/java/com/codecool/umbrella/api/client/CoordinatesClient.java
+++ backend/src/main/java/com/codecool/umbrella/api/client/CoordinatesClient.java
@@ -1,35 +1,21 @@
 package com.codecool.umbrella.api.client;
 
+import com.codecool.umbrella.logic.WebClientService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
 
 @Controller
 public class CoordinatesClient {
     private final String api;
+    private WebClientService webClientService;
 
-    public CoordinatesClient(@Value("${api.geocoding}") String api) {
+    public CoordinatesClient(@Value("${api.geocoding}") String api, WebClientService webClientService) {
         this.api = api;
+        this.webClientService = webClientService;
     }
 
-    //TODO: Upgrade this method to use WebClient instead (not URL+openConnection)
     public String getBy(String name) {
         String params = "?name=" + name;
-        try {
-            URLConnection connection = new URL(api + params).openConnection();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
-            String lines = reader.lines().collect(Collectors.joining("\n"));
-            reader.close();
-            return lines;
-        } catch (IOException error) {
-            throw new RuntimeException(error);
-        }
+        return webClientService.getOne(api, params);
     }
 }
--- backend/src/main/java/com/codecool/umbrella/api/client/WeatherClient.java
+++ backend/src/main/java/com/codecool/umbrella/api/client/WeatherClient.java
@@ -1,25 +1,19 @@
 package com.codecool.umbrella.api.client;
 
+import com.codecool.umbrella.logic.WebClientService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
 
 @Controller
 public class WeatherClient {
     private final String api;
+    private WebClientService webClientService;
 
-    public WeatherClient(@Value("${api.weather}") String api) {
+    public WeatherClient(@Value("${api.weather}") String api, WebClientService webClientService) {
         this.api = api;
+        this.webClientService = webClientService;
     }
 
-    //TODO: Upgrade this method to use WebClient instead (not URL+openConnection)
     public String getBy(String coordinates) {
         String latitude = coordinates.split(",")[0];
         String longitude = coordinates.split(",")[1];
@@ -28,14 +22,6 @@ public class WeatherClient {
                 , latitude
                 , longitude
         );
-        try {
-            URLConnection connection = new URL(api + params).openConnection();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8));
-            String lines = reader.lines().collect(Collectors.joining("\n"));
-            reader.close();
-            return lines;
-        } catch (IOException error) {
-            throw new RuntimeException(error);
-        }
+        return webClientService.getOne(api, params);
     }
 }
