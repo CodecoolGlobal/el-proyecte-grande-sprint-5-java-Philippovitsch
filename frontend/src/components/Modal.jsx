6287be45bf82f9d343e8599d3f032567c51385b0
Philippovitsch <philipp@stadter.at>
2023-02-23 16:08:52 +0100

    feat: add picture to location modal

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -4,6 +4,7 @@ import Typography from '@mui/material/Typography';
 import List from '@mui/material/List';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import Grid from '@mui/material/Grid';
 
 import { fetchFunFact } from '../functions/fetch';
 import { Box } from '@mui/system';
@@ -33,35 +34,50 @@ export default function Modal({ closeModal, locationData }) {
   return (
     <div className="modal" onClick={closeModal}>
       <div className="modal-content" onClick={(event) => {event.stopPropagation()}} style={{cursor: 'default'}}>
-        <span className="close" onClick={closeModal}>&times;</span>
-        <Typography variant="h3" component="div" sx={{ p: 2 }}>
-            {locationData.location}
-          </Typography>
-        <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-          <ListItemButton style={{cursor: 'default'}}>
-            <ListItemText><b>Country: </b>{locationData.country}</ListItemText>
-          </ListItemButton>
-          <ListItemButton style={{cursor: 'default'}}>
-            <ListItemText><b>Temperature: </b>{locationData.temperature}</ListItemText>
-          </ListItemButton>
-          <ListItemButton style={{cursor: 'default'}}>
-            <ListItemText><b>Weather code: </b>{locationData.weatherCode}</ListItemText>
-          </ListItemButton>
-          <ListItemButton style={{cursor: 'default'}}>
-            <ListItemText><b>Wind direction: </b>{locationData.windDirection}°</ListItemText>
-          </ListItemButton>
-          <ListItemButton style={{cursor: 'default'}}>
-            <ListItemText><b>Wind speed: </b>{locationData.windSpeed}</ListItemText>
-          </ListItemButton>
-        </List>
-
-        <Box className="fun-fact">
-          <Typography component="div" sx={{ p: 2 }}>
-            <em>Fun fact about <b>{locationData.location} </b>
-            <span style={{ color: "#6F7378" }}>(by OpenAI)</span>:</em><br />
-            {funFact}
-          </Typography>
-        </Box>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <span className="close" onClick={closeModal}>&times;</span>
+            <Typography variant="h3" component="div" sx={{ p: 2 }}>
+              {locationData.location}
+            </Typography>
+          </Grid>
+          <Grid item xs={4.3}>
+            <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+              <ListItemButton style={{cursor: 'default'}}>
+                <ListItemText><b>Country: </b>{locationData.country}</ListItemText>
+              </ListItemButton>
+              <ListItemButton style={{cursor: 'default'}}>
+                <ListItemText><b>Temperature: </b>{locationData.temperature}</ListItemText>
+              </ListItemButton>
+              <ListItemButton style={{cursor: 'default'}}>
+                <ListItemText><b>Weather code: </b>{locationData.weatherCode}</ListItemText>
+              </ListItemButton>
+              <ListItemButton style={{cursor: 'default'}}>
+                <ListItemText><b>Wind direction: </b>{locationData.windDirection}°</ListItemText>
+              </ListItemButton>
+              <ListItemButton style={{cursor: 'default'}}>
+                <ListItemText><b>Wind speed: </b>{locationData.windSpeed}</ListItemText>
+              </ListItemButton>
+            </List>
+          </Grid>
+          <Grid item xs={7.7}>
+            <img
+              style={{ display: "inline-block", marginTop: 10 }}
+              src="https://www.sumomag.at/wp-content/uploads/2016/02/Corporate-Publishing_1-e1455555060462.png"
+              alt="placeholder"
+              width={450}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Box className="fun-fact">
+              <Typography component="div" sx={{ p: 2 }}>
+                <em>Fun fact about <b>{locationData.location} </b>
+                <span style={{ color: "#6F7378" }}>(by OpenAI)</span>:</em><br />
+                {funFact}
+              </Typography>
+            </Box>
+          </Grid>
+        </Grid>
       </div>
     </div>
   )
