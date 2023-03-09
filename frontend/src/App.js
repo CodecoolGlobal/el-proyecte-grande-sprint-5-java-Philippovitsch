0a7fc3f6cd1c36d9be6c71a7154021bc73cb8f22
jurijkreutz <jurij.kreutz97@gmail.com>
2023-03-09 10:34:49 +0100

    feat: add calendar without functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Events from './pages/Events';
 
 export default function App() {
 
@@ -8,6 +9,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/About" element={<About />} />
+      <Route path="/Events" element={<Events />} />
     </Routes>
   );
 }
---
+++ frontend/src/components/Calendar.jsx
@@ -0,0 +1,30 @@
+import { Card, CardContent, Typography } from '@mui/material'
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { DatePicker } from '@mui/lab';
+
+import React from 'react'
+
+export default function Calendar( {handleCalendarClick} ) {
+  const adapter = new AdapterDayjs();
+  const twoWeeksInMs = 12096e5
+  const maxDate = adapter.date(Date.now() + twoWeeksInMs);
+
+  return (
+    <div>
+      <Card sx={{ pt: 3.5, pr: 3.5, pl: 3.5 }}>
+        <CardContent>
+          <Typography className="location"
+            variant="body1"
+            sx={{ textAlign: 'center', mb: 2 }}>
+            Event Calendar
+          </Typography>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateCalendar disablePast maxDate={maxDate} onChange={handleCalendarClick}/>
+          </LocalizationProvider>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
