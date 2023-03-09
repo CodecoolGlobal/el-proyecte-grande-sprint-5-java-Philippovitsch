68b56d92b937ee673da4e87a2fbe49a14ad19098
LukasJandl <jandl.lukas@gmail.com>
2023-03-09 11:39:40 +0100

    feat: added team description to about page

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,3 +1,9 @@
 export default function About() {
-    return <h1>About</h1>
+    return (
+        <div className="about">
+            The Umbrella Corporation was founded on January 30th 2023 so that
+            three coding amateurs could finish the last stage of their training
+            to finaly get a job and do something with there lives.
+        </div>
+    )
 }
