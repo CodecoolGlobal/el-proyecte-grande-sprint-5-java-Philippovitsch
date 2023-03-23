5f4822b1422240f682de2047832335e50d9a01e2
Philippovitsch <philipp@stadter.at>
2023-03-23 09:41:55 +0100

    bugfix: Revert same site allowed, secure = true

--- backend/src/main/java/com/codecool/umbrella/security/jwt/JwtUtils.java
+++ backend/src/main/java/com/codecool/umbrella/security/jwt/JwtUtils.java
@@ -43,8 +43,6 @@ public class JwtUtils {
                 .path("/api")
                 .maxAge(24*60*60)
                 .httpOnly(true)
-                .sameSite("None")
-                .secure(true)
                 .build();
         return cookie;
     }
