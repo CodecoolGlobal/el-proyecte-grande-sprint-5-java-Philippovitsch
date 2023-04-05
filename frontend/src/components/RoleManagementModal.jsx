7d2798fbc6086774f1a178fac6cf54b41c465453
jurijkreutz <jurij.kreutz97@gmail.com>
2023-04-05 11:04:30 +0200

    implement 90% of frontend for role management

---
+++ frontend/src/components/RoleManagementModal.jsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { useState } from 'react';
+import { Typography, FormLabel, RadioGroup, FormControl, FormControlLabel, Radio, Grid, Box, Button } from '@mui/material'
+
+export default function RoleManagementModal({ closeModal, userToEditRole, oldRole }) {
+
+    const [newRole, setNewRole] = useState(oldRole);
+
+    function changeUserRoleHandler(userToEditRole, newRole) {
+        // await removeUser(username);
+        console.log(userToEditRole + " " + newRole)
+    }
+
+  return (
+    <div className="modal" onClick={closeModal}>
+      <div  className="modal-content" 
+            sx = {{ p: 5 }}
+            onClick={(event) => { event.stopPropagation() }} 
+            style={{ cursor: 'default' }}>
+        <Typography gutterBottom variant="h4" component="div" sx={{ mt: 4, textAlign: "center" }}>
+           Role Management
+        </Typography>
+        <Typography gutterBottom variant="h5" component="div" sx={{ mt: 2, mb: 7, textAlign: "center" }}>
+           User: {userToEditRole}
+        </Typography>
+        <Grid container spacing={2}>
+            <Grid item xs={6}>
+                <Box sx={{ textAlign: 'center' }}>
+                    <FormControl>
+                        <FormLabel id="role-radio-label">Choose role:</FormLabel>
+                            <RadioGroup
+                                aria-labelledby="role-radio-label"
+                                defaultValue="user"
+                                name="radio-buttons-group"
+                                onChange={(event) => setNewRole(event.target.value)}
+                            >
+                                <FormControlLabel value="USER" control={<Radio />} label="Standard User" />
+                                <FormControlLabel value="PREMIUM_USER" control={<Radio />} label="Premium User" />
+                                <FormControlLabel value="ADMIN" control={<Radio />} label="Administrator" />
+                            </RadioGroup>
+                    </FormControl>
+                </Box>
+            </Grid>
+            <Grid item xs={6}>
+                <Box sx={{ textAlign: 'center' }}>
+                    <Typography gutterBottom variant="h5" component="div" sx={{ m: 4, mt: 2, textAlign: "center" }}>
+                        <p style={{color: '#adadad'}}>Old Role: {oldRole}</p>
+                        <p>New Role: {newRole}</p>
+                    </Typography>
+                </Box>
+            </Grid>
+        </Grid>
+        <Box sx={{ textAlign: 'center', mt: 5 }}>
+            <Button variant="contained" onClick={() => changeUserRoleHandler(userToEditRole, newRole)}>Submit</Button>
+        </Box>
+      </div>
+    </div>
+  )
+}
--- frontend/src/components/UsersTable.jsx
+++ frontend/src/components/UsersTable.jsx
@@ -4,55 +4,85 @@ import { Table, TableHead, TableRow, TableCell, Paper, TableBody } from "@mui/ma
 import { fetchData } from '../functions/fetch';
 import { Button } from '@mui/material';
 import { removeUser } from '../functions/fetch';
+import RoleManagementModal from './RoleManagementModal';
 
 export default function UsersTable() {
 
     const [users, setUsers] = useState([]);
     const [deleteCounter, setDeleteCounter] = useState(0);
+    const [openModal, setOpenModal] = useState(false);
+    const [userToEditRole, setUserToEditRole] = useState('');
+    const [oldRoleOfUserToEdit, setOldRoleOfUserToEdit] = useState('')
 
     async function removeUserHandler(username) {
         await removeUser(username);
         setDeleteCounter(deleteCounter+1);
     }
 
+    function removeRole_Prefix(fetchedUsers) {
+        fetchedUsers.forEach(element => {
+            element.roles[0].name = element.roles[0].name.substring(5);
+        });
+    }
+
     useEffect(() => {
         async function fetchUsers() {
             const fetchedUsers = await fetchData("/api/admin");
+            removeRole_Prefix(fetchedUsers);
             setUsers(fetchedUsers);
         }
         fetchUsers();
     }, [deleteCounter])
 
+    const closeModal = () => {
+        setOpenModal(false);
+    }
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Username</TableCell>
-            <TableCell align="left">E-Mail</TableCell>
-            <TableCell align="left">Role</TableCell>
-            <TableCell align="left">Administration</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {users.map((row) => (
-            <TableRow
-              key={row.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell align="left">{row.username}</TableCell>
-              <TableCell align="left">{row.email}</TableCell>
-              <TableCell align="left">{row.roles[0].name}</TableCell>
-              <TableCell align="left">
-                <Button variant="text"
-                        onClick={() => { 
-                            removeUserHandler(row.username);
-                            }}>Delete</Button>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                <TableRow>
+                    <TableCell>Username</TableCell>
+                    <TableCell align="left">E-Mail</TableCell>
+                    <TableCell align="left">Role</TableCell>
+                    <TableCell align="left">Administration</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                {users.map((row) => (
+                    <TableRow
+                    key={row.id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                    <TableCell align="left">{row.username}</TableCell>
+                    <TableCell align="left">{row.email}</TableCell>
+                    <TableCell align="left">{row.roles[0].name}</TableCell>
+                    <TableCell align="left">
+                        <Button variant="text"
+                                onClick={() => { 
+                                    removeUserHandler(row.username);
+                                    }}>Delete</Button>
+                        <Button variant="text"
+                                onClick={() => { 
+                                    setUserToEditRole(row.username);
+                                    setOldRoleOfUserToEdit(row.roles[0].name)
+                                    setOpenModal(true);
+                                    }}>Change Role</Button>
+                    </TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+        {openModal && 
+            <RoleManagementModal 
+                closeModal={closeModal}
+                userToEditRole={userToEditRole} 
+                oldRole = {oldRoleOfUserToEdit}
+            />
+        }
+    </>
   )
 }
