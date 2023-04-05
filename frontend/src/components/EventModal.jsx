ceebfb33301cf0002b13d1961c808af91bf2f78f
Philippovitsch <philipp@stadter.at>
2023-04-05 13:38:39 +0200

    style: improve event cards

--- frontend/src/components/EventModal.jsx
+++ frontend/src/components/EventModal.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 
 import Grid from '@mui/material/Grid';
-import { Box, Typography } from '@mui/material';
+import { CircularProgress, Typography } from '@mui/material';
 import { TextField } from '@mui/material';
 import { Button } from '@mui/material';
 
@@ -11,7 +11,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import dayjs from 'dayjs';
 import { fetchData } from '../functions/fetch';
 import { fetchDailyData } from '../functions/fetch';
-import { WIND_ICON, getWeatherIcon } from './WeatherCard';
+import WeatherForecast from './WeatherForecast';
 
 export default function EventModal({ closeModal, addCalendarEvent, date }) {
 
@@ -112,46 +112,10 @@ export default function EventModal({ closeModal, addCalendarEvent, date }) {
             </LocalizationProvider>
             <Button variant="contained" onClick={onAdd} sx={{ mt: 6,  ml: 1 }}>SUBMIT</Button>
           </Grid>
-          <Grid item xs={6} sx={{ mt: 2 }}>
-            {weatherForecast &&
-              <>
-                <Typography className="temperature"
-                  variant="body1"
-                  sx={{
-                    textAlign: 'center',
-                    mb: 2, mt: 2,
-                    fontSize: 20
-                  }}>
-                    Min: <b>{weatherForecast.temperature_2m_min}{(weatherForecast.temperature_2m_min[0] !== "n.a.") && "°C"}</b> / 
-                    Max: <b>{weatherForecast.temperature_2m_max}{(weatherForecast.temperature_2m_min[0] !== "n.a.") && "°C"}</b>
-                </Typography>
-                <Box sx={{ textAlign: 'center' }}>
-                  <img
-                    className="weather-icon"
-                    src={getWeatherIcon(weatherForecast.weathercode[0])}
-                    alt='weather icon'
-                    width='100' />
-                  <br />
-                  <br />
-                </Box>
-                <Box
-                  sx={{ textAlign: 'center', mb: 2 }}>
-                  <img
-                    className="wind-icon"
-                    src={WIND_ICON}
-                    alt="wind icon" />
-                </Box>
-                <Typography className="windspeed"
-                  variant="body1"
-                  sx={{ textAlign: 'center', mb: 2 }}>
-                  {weatherForecast.windspeed_10m_max}{(weatherForecast.windspeed_10m_max[0] !== "n.a.") && " km/h"}
-                </Typography>
-                <Typography className="location"
-                  variant="body1"
-                  sx={{ textAlign: 'center', mb: 2 }}>
-                    <i>{country + ", " + location}</i>
-                </Typography> 
-              </>
+          <Grid item xs={6} sx={{ mt: 2 }} justifyContent="center" display={!weatherForecast && "flex"}>
+            {weatherForecast
+              ? <WeatherForecast weatherForecast={weatherForecast} country={country} location={location} />
+              : <CircularProgress sx={{ mt: 4 }}/>
             }
           </Grid>
         </Grid>
