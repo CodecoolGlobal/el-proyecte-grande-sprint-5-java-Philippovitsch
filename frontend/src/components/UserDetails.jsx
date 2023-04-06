2a0f755f9eb99361871f950337b82b1e4e54b8cb
Philippovitsch <philipp@stadter.at>
2023-04-06 15:07:09 +0200

    style: improve design of user details page

--- frontend/src/components/UserDetails.jsx
+++ frontend/src/components/UserDetails.jsx
@@ -5,36 +5,38 @@ import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 
-import { Typography } from '@mui/material'
+import { Card, Typography } from '@mui/material'
 
 export default function UserDetails({userData}) {
   return (
-    <div className="content-wrapper">
-      <Typography gutterBottom variant="h4" component="div">
-        User details:
-      </Typography>
-      <TableContainer component={Paper} sx={{ width: 500, marginTop: 4 }}>
-        <Table aria-label="simple table">
-          <TableBody>
-            <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell><b>User-ID: </b></TableCell>
-              <TableCell>{userData.id}</TableCell>
-            </TableRow>
-            <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell><b>Username: </b></TableCell>
-              <TableCell>{userData.username}</TableCell>
-            </TableRow>
-            <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell><b>E-Mail: </b></TableCell>
-              <TableCell>{userData.email}</TableCell>
-            </TableRow>
-            <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell><b>Roles: </b></TableCell>
-              <TableCell>{userData.roles.join(", ")}</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-      </TableContainer>
+    <div className="cardwrapper">
+      <Card sx={{ width: 1, pt: 3.5, pr: 3.5, pl: 3.5 }}>
+        <Typography gutterBottom variant="h4" component="div">
+          User details:
+        </Typography>
+        <TableContainer component={Paper} sx={{ width: 500, marginTop: 4 }}>
+          <Table aria-label="simple table">
+            <TableBody>
+              <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell><b>User-ID: </b></TableCell>
+                <TableCell>{userData.id}</TableCell>
+              </TableRow>
+              <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell><b>Username: </b></TableCell>
+                <TableCell>{userData.username}</TableCell>
+              </TableRow>
+              <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell><b>E-Mail: </b></TableCell>
+                <TableCell>{userData.email}</TableCell>
+              </TableRow>
+              <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                <TableCell><b>Roles: </b></TableCell>
+                <TableCell>{userData.roles.join(", ")}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Card>
     </div>
   )
 }
